60f57aaf5c55ad5625f6a69572bb6c015363d516
zktm9903 <zktm9903@naver.com>
2024-05-01 16:59:36 +0900

    [feat] TopBar 추가, dayjs 의존성 추가

--- src/MacOs.tsx
+++ src/MacOs.tsx
@@ -1,6 +1,7 @@
 import './index.css';
 import Dock from './commons/Dock';
 import { css } from '@emotion/react';
+import TopBar from './commons/TopBar';
 
 export default function MacOs() {
   return (
@@ -13,6 +14,7 @@ export default function MacOs() {
         backgroundImage:
           'url("https://images.pexels.com/photos/1428277/pexels-photo-1428277.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2")',
       })}>
+      <TopBar />
       <Dock />
     </div>
   );
---
+++ src/commons/TopBar.tsx
@@ -0,0 +1,37 @@
+import { css } from '@emotion/react';
+import dayjs from 'dayjs';
+import { useEffect, useState } from 'react';
+
+export default function TopBar() {
+  return (
+    <div
+      css={css({
+        position: 'fixed',
+        top: 0,
+        left: 0,
+        right: 0,
+        display: 'flex',
+        justifyContent: 'space-between',
+        color: 'white',
+        padding: '4px 16px 4px 16px',
+        backgroundColor: 'rgba(0, 0, 0, 0.3)',
+        backdropFilter: 'blur(10px)',
+      })}>
+      <OsName />
+      <DateTime />
+    </div>
+  );
+}
+
+function OsName() {
+  return <div>lee-sang</div>;
+}
+
+function DateTime() {
+  const [dateTime, setDateTime] = useState(new Date());
+
+  useEffect(() => {
+    setInterval(() => setDateTime(new Date()), 1000);
+  }, []);
+  return <div>{dayjs(dateTime).format('ddd MMMM D h:mm A')}</div>;
+}

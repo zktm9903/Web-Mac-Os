dfa359097e04125c505c7977035ba482611118ce
zktm9903 <zktm9903@naver.com>
2024-05-27 00:51:18 +0900

    vercel 배포테스트

--- src/apps/apps.tsx
+++ src/apps/apps.tsx
@@ -1,18 +1,18 @@
 import { APP } from '../types/os';
-import MemoIcon from './memo/Icon';
-import MemoContent from './memo/Content';
-import ProfileIcon from './profile/Icon';
-import ProfileContent from './profile/Content';
+import Icon from './memo/Icon';
+import Content from './memo/Content';
+// import ProfileIcon from './profile/Icon';
+// import ProfileContent from './profile/Content';
 
 export const APPS: APP[] = [
   {
     name: 'memo',
-    icon: <MemoIcon />,
-    content: <MemoContent />,
+    icon: <Icon />,
+    content: <Content />,
   },
-  {
-    name: 'profile',
-    icon: <ProfileIcon />,
-    content: <ProfileContent />,
-  },
+  // {
+  //   name: 'profile',
+  //   icon: <ProfileIcon />,
+  //   content: <ProfileContent />,
+  // },
 ];
